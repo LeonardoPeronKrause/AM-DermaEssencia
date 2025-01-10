c39590b251b994ba41e7f65b6ea913622c5a950e
Leonardo Peron Krause <lperonkrause@gmail.com>
2025-01-10 12:02:17 -0300

    Finalizei o projeto

--- src/script.js
+++
@@ -1,9 +0,0 @@
-// Redireciona para o WhatsApp ao clicar no botão
-document.querySelectorAll('.btn, .btn1').forEach(button => {
-    button.addEventListener('click', () => {
-        const phoneNumber = "5548996917300"; // Substitua pelo número de telefone correto
-        const message = "Olá! Gostaria de agendar uma consulta.";
-        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-        window.open(url, '_blank');
-    });
-});
